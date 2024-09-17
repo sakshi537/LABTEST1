7ce4fca02e7d7b4349a343adf274e8c9d320cd75
sakshi537 <22103076@mail.jiit.ac.in>
2024-09-17 16:20:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy import special ,linalg
+np.array =['flight no','departure staion','arrival station','departure time','available seats']
+np.array =['103','kota','udaipur','9:20','10:15']
+print(array);
+
