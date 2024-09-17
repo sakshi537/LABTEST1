00d2c98b806f9a0db481742a6f26c4ad2aae9b75
sakshi537 <22103076@mail.jiit.ac.in>
2024-09-17 16:21:47 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy import special
+flight_seats =int(input("enter the past avialble seats"))
+print(flight_seats)
+#update the avaialble flight seats
+flight_seats =int(input("enter the updated available seats"))
+print(flight_seats)
