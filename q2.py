ab5264c63ca71e17b8fac74fe5e8f5b0b23f7fbb
sakshi537 <22103076@mail.jiit.ac.in>
2024-09-17 16:21:15 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import special
+ID = int(input("enter the id"))
+print(ID)
+passenger_name = str(input("enter the name"))
+print(passenger_name)
+reservation_list =int(input("flight number"))
+print(reservation_list)
